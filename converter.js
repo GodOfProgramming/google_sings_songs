efdc88ce574ed7f5fa29674e6aa2e9bff689b381
GodOfProgramming <jonathan.dickert@protonmail.com>
2022-06-24 00:04:31 -0400

    script works

--- converter.js
+++ converter.js
@@ -1,3 +1,36 @@
-const translate = require('google-translate-api');
+const translate = require('google-translate-api-x');
+const fs = require('fs');
 
-translate("hello", { from: 'en', to: 'es' });
+const file = process.argv[2];
+const times = process.argv[3] || 10;
+
+async function main(file, times) {
+  if (!!file) {
+    let song = fs.readFileSync(`raw/${file}`, { encoding: 'utf8', flag: 'r' });
+
+    if (!!song) {
+      const langs = Object.keys(translate.languages);
+
+      let last = 'en';
+
+      for (let i = 0; i < times; ++i) {
+        const next = langs[Math.floor(Math.random() * langs.length)];
+
+        const res = await translate(song, { from: last, to: next });
+
+        song = res.text;
+        last = next;
+      }
+
+      const res = await translate(song, { from: last, to: 'en' });
+
+      fs.writeFileSync(`conv/${file}`, res.text, { encoding: 'utf8', flag: 'w+' });
+    } else {
+      console.log("file was empty");
+    }
+  } else {
+    console.log("no file given");
+  }
+}
+
+main(file, times);
