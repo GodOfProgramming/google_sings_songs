55916f3886dd3b936fe2289b682e7db4df25ee62
GodOfProgramming <jonathan.dickert@protonmail.com>
2022-06-25 00:28:25 -0400

    more

--- converter.js
+++ converter.js
@@ -9,19 +9,21 @@ async function main(file, times) {
     let song = fs.readFileSync(`raw/${file}`, { encoding: 'utf8', flag: 'r' });
 
     if (!!song) {
-      const langs = Object.keys(translate.languages);
+      const langs = Object.keys(translate.languages).filter((k) => isNaN(Number(k)) && k !== 'auto' && k !== 'isSupported' && k !== 'getCode');
 
       let last = 'en';
 
       for (let i = 0; i < times; ++i) {
         const next = langs[Math.floor(Math.random() * langs.length)];
 
+        console.log(`converting ${translate.languages[last]} to ${translate.languages[next]}`);
         const res = await translate(song, { from: last, to: next });
 
         song = res.text;
         last = next;
       }
 
+      console.log(`converting ${translate.languages[last]} to English`);
       const res = await translate(song, { from: last, to: 'en' });
 
       fs.writeFileSync(`conv/${file}`, res.text, { encoding: 'utf8', flag: 'w+' });
